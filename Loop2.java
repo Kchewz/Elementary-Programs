817c6dacb90b5b3a41ce27d0600a0c8a8d8f1be4
Kenneth Chyzewski <KChyzewski@gmail.com>
2018-09-07 10:58:17 -0400

    Create Loop2.java

---
+++ Loop2.java
@@ -0,0 +1,41 @@
+/*
+ * Program: Loop2.java
+ * Author: Kenny Chyzewski
+ * Description: A program that will take a whole number and will print the corresponding number of * in a vertical line.
+ */
+
+//This is required for command line input
+import java.io.*;
+public class Loop2
+  //^ This line of code opens the class
+{
+  public static void main(String[] args)
+    //^ This line of code opens the main
+  {
+    System.out.println("This prints the number of * as the whole number inputted.");
+    
+    //All variable are kept at the top for neatnesss
+    InputStreamReader isr = new InputStreamReader(System.in); 
+    BufferedReader br = new BufferedReader(isr);
+    String s = ""; //Create a string to support what the user inputs
+    int number = 0;
+    
+    try      
+    {
+      //This wil print the following sentence when running the program
+      System.out.println("Enter a number.");
+      s = br.readLine();
+      number = Integer.parseInt(s);      
+      
+    }
+    //This will support the program if try fails
+    catch(IOException ex)
+    {
+      ex.printStackTrace();
+      
+    }
+    for (int loop = 0; loop < number; loop++) //This loop uses the check-do-math method to solve how many * are needed to be outputted.
+      
+      System.out.println("*"); //Must be .println in order to print results vertically.
+  }
+}
